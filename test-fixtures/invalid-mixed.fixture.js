deecefdaee0b29e69297b5c7a8752a9e796fc254
Paul Melnikow <email@paulmelnikow.com>
2019-04-07 12:58:19 -0500

    [DEPLOY] Completely clean the index

--- test-fixtures/invalid-mixed.fixture.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const BaseJsonService = require('../core/base-service/base-json')
-
-class BadBaseService {}
-class GoodService extends BaseJsonService {}
-class BadService extends BadBaseService {}
-
-module.exports = [GoodService, BadService]
